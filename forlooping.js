6e4f8cf68b85045f5d244691170231bb5d4b0f73
rudysantoso <63795450+rudysantoso@users.noreply.github.com>
2020-04-20 06:18:14 +0700

    Add files via upload

--- forlooping.js
+++ forlooping.js
@@ -1,1 +1,11 @@
-
+console.log('perulangan petama')
+var loop = 2 ;
+while(loop < 20){
+  console.log(loop + ' I love coding');
+loop++
+}
+console.log('perulangan kedua')
+while(loop >= 2){
+  console.log(loop + ' I will become fullstack javascript developer');
+loop--
+}
