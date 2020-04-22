f809f830c473dc6c3faa41fee5b3961acec6f4e8
rudysantoso <rudysantoso1702@gmail.com>
2020-04-22 07:10:48 +0700

    tambahan file

---
+++ forlooping2.js
@@ -0,0 +1,32 @@
+for(let i=0;i<=100;i++){
+    if((i%2)==1){
+        console.log('ganjil');
+    } else {
+        console.log('genap');
+      }
+  }
+  
+  for(let i=1;i<=100;i+=2){
+    if((i%3)===0){
+        console.log(i + ' kelipatan 3')
+    } else {
+        console.log('')
+    }
+  }
+  
+  for(let i=1;i<=100;i+=5){
+    if((i%6)===0){
+        console.log(i + ' kelipatan 6')
+    } else {
+        console.log('')
+    }
+  }
+  
+  for(let i=1;i<=100;i+=9){
+    if((i%10)===0){
+        console.log(i + ' kelipatan 10')
+    } else {
+        console.log('')
+    }
+  }
+  
